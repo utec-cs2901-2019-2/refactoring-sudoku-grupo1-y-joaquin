c694e4d8e40ca92ee0cb2556cb39d88b7e67713c
Herless Alvarado <herlessalvarado@Herlesss-MacBook-Pro.local>
2019-10-10 12:02:43 -0500

    print matrix added

--- Sudoku/src/com/company/Sudoku.java
+++ Sudoku/src/com/company/Sudoku.java
@@ -31,7 +31,10 @@ public class Sudoku {
     }
 
     public void showMatrix(int [][] matrix){
-
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
     }
 
     public void verify(){
